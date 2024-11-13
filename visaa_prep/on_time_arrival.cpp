cce093be8ebec9788605bf88f2de923bef1f77da
jhansirudroju <126376692+jhansirudroju@users.noreply.github.com>
2024-11-13 20:40:21 +0530

    Create on_time_arrival.cpp

---
+++ visaa_prep/on_time_arrival.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int x;
+        cin>>x;
+        if(x >= 30) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
